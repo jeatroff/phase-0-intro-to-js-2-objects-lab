42c0867fcb7de1f9961155736011b5dd4a9c3fed
Jonathan Eatroff <jonathan.eatroff@gmail.com>
2022-11-08 00:13:33 -0500

    commit tree 93a9f1cb1217d5362cced3aefc4f3a71280d94c5

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var employee = {
+    name: undefined,
+    streetAddress: undefined
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    var newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    var newEmployee = {...employee};
+    newEmployee[key] = undefined;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    employee[key] = undefined;
+    return employee;
+}
